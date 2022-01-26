ec521e54353cdd952a6132173059f6165ef61c4c
vazsonyidl <danielvazsonyi@gmail.com>
2022-01-26 20:22:38 +0200

    feat(footer): create footer component

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.scss';
+
+export default function Footer() {
+    return (
+        <footer role='term' className={styles.footer}>
+            <section className={styles.description}>
+                Made with coffee and Next.js.
+            </section>
+        </footer>
+    )
+}
