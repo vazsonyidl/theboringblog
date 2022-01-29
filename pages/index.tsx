0bbbbbd6771959a1247be52ae3dcf62ba97df487
vazsonyidl <danielvazsonyi@gmail.com>
2022-01-29 12:56:35 +0200

    build(project): add favicons, icon and landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { parse } from 'date-fns';
 
 import Header from 'components/header/Header';
 import Footer from 'components/footer/Footer';
@@ -7,11 +8,31 @@ import { IPost } from 'interfaces/post.interface';
 import { navigationElements } from 'constants/navigation.const';
 import { posts } from 'constants/posts.const';
 
-export default function Home({posts}: {posts: Array<IPost>}) {
+export default function Home({ posts }: { posts: Array<IPost> }) {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/static/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/static/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/static/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/static/site.webmanifest" />
+        <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <Header navigationElements={navigationElements} />
       <main>
@@ -23,9 +44,15 @@ export default function Home({posts}: {posts: Array<IPost>}) {
 }
 
 export async function getStaticProps() {
+  const _posts = posts.sort(
+    (a, b) =>
+      parse(b.published, 'dd.MM.yyyy', new Date()).getTime() -
+      parse(a.published, 'dd.MM.yyyy', new Date()).getTime()
+  );
+
   return {
     props: {
-      posts,
+      posts: _posts,
     },
-  }
+  };
 }
