18eb1d93d84706d86ca458e66cfea6381c86524c
vazsonyidl <danielvazsonyi@gmail.com>
2022-02-04 19:34:49 +0200

    fix(seo): add meta description and robots.txt for crawlers

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,32 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang='en'>
-        <Head />
+        <Head>
+        <title>Longterm earnings</title>
+        <meta property="og:title" content="Longterm earnings" key="title" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/static/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/static/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/static/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/static/site.webmanifest" />
+        <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name='description' content='How to earn passive income during a long term period? How to manage your long term investment? The long term earnings blog give you insights!'/>   
+      </Head>
         <body>
           <Main />
           <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import { parse } from 'date-fns';
 
 import Header from 'components/header/Header';
@@ -11,31 +10,6 @@ import { posts } from 'constants/posts.const';
 export default function Home({ posts }: { posts: Array<IPost> }) {
   return (
     <>
-      <Head>
-        <title>Longterm earnings</title>
-        <meta property="og:title" content="Longterm earnings" key="title" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/static/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/static/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/static/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/static/site.webmanifest" />
-        <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
       <Header navigationElements={navigationElements} />
       <main>
         <PostList posts={posts} />
