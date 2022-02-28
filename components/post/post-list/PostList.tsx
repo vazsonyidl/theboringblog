b07a1a6545cd9bab47d59fd7015bd4df56a8e35d
vazsonyidl <danielvazsonyi@gmail.com>
2022-02-28 18:24:05 +0200

    post - update style and color palette

--- components/post/post-list/PostList.tsx
+++ components/post/post-list/PostList.tsx
@@ -4,7 +4,6 @@ import styles from './PostList.module.scss';
 export default function PostsList({ posts }: {posts: Array<{content: string, meta: any, slug: string}>}) {
   return (
     <>
-    <section role='dialog'></section>
     <section role="table" className={styles.postContainer}>
       {posts.map((post) => (
         <PostListItem meta={post.meta} slug={post.slug} key={post.slug} />
